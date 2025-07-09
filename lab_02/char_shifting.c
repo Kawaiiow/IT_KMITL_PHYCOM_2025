4a5d61a7c1fde6541c729b0e2427291a475ca0db
Kawaiiow <poohsn1357@gmail.com>
2025-07-09 17:16:19 +0700

    avoid newline

--- lab_02/char_shifting.c
+++ lab_02/char_shifting.c
@@ -7,11 +7,16 @@ int main(void)
 	char	c2;
 	char	c3;
 	char	c4;
+	char	nu;
 
 	scanf("%c", &c);
+	scanf("%c", &nu);
 	scanf("%c", &c1);
+	scanf("%c", &nu);
 	scanf("%c", &c2);
+	scanf("%c", &nu);
 	scanf("%c", &c3);
+	scanf("%c", &nu);
 	scanf("%c", &c4);
 	printf("%c\n%c\n%c\n%c\n%c", c + 1, c1, c2 +1, c3, c4 + 1);
 }
