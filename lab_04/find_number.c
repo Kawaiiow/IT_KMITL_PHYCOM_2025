578b1850cc67c36a79160914344d74a27cbf78fb
Kawaiiow <poohsn1357@gmail.com>
2025-07-23 09:59:15 +0700

    fix find number

--- lab_04/find_number.c
+++ lab_04/find_number.c
@@ -11,7 +11,7 @@ int	main()
 		printf("%.2f\n", w1);
 	else if ((w2 > w1 && w2 < w3) || (w2 < w1 && w1 > w3))
 		printf("%.2f\n", w2);
-	else
+	else if ((w3 > w2 && w3 < w1) || (w3 < w2 && w3 > w1))
 		printf("%.2f\n", w3);
 	return (0);
 }
