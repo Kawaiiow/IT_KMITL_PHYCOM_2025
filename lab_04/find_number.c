5256f7ccbd343c89775afbcca874c2f72efe6717
Kawaiiow <poohsn1357@gmail.com>
2025-07-23 09:58:07 +0700

    add find number

---
+++ lab_04/find_number.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int	main()
+{
+	float	w1;
+	float	w2;
+	float	w3;
+
+	scanf("%f %f %f", &w1, &w2, &w3);
+	if ((w1 > w2 && w1 < w3) || (w1 < w2 && w1 > w3))
+		printf("%.2f\n", w1);
+	else if ((w2 > w1 && w2 < w3) || (w2 < w1 && w1 > w3))
+		printf("%.2f\n", w2);
+	else
+		printf("%.2f\n", w3);
+	return (0);
+}
