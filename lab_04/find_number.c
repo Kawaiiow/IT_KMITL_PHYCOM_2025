2fbb05b40e5f2e43698e06e7ef505ea93a221c02
Kawaiiow <poohsn1357@gmail.com>
2025-07-23 10:01:37 +0700

    fix find number

--- lab_04/find_number.c
+++ lab_04/find_number.c
@@ -7,11 +7,13 @@ int	main()
 	float	w3;
 
 	scanf("%f %f %f", &w1, &w2, &w3);
-	if ((w1 > w2 && w1 < w3) || (w1 < w2 && w1 > w3))
+	if ((w1 >= w2 && w1 <= w3) || (w1 <= w2 && w1 >= w3))
 		printf("%.2f\n", w1);
-	else if ((w2 > w1 && w2 < w3) || (w2 < w1 && w1 > w3))
+	else if ((w2 >= w1 && w2 <= w3) || (w2 <= w1 && w1 >= w3))
 		printf("%.2f\n", w2);
-	else if ((w3 > w2 && w3 < w1) || (w3 < w2 && w3 > w1))
+	else if ((w3 >= w2 && w3 <= w1) || (w3 <= w2 && w3 >= w1))
+		printf("%.2f\n", w3);
+	else
 		printf("%.2f\n", w3);
 	return (0);
 }
