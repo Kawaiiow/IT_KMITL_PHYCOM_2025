a483289f73515eb8de003dfea5152b49bce2a62d
Kawaiiow <poohsn1357@gmail.com>
2025-07-23 10:32:23 +0700

    Don't ever try read this

--- lab_04/your_score.c
+++ lab_04/your_score.c
@@ -1,11 +1,3 @@
 #include <stdio.h>
 
-int	main()
-{
-	float	score;
-
-	score = 0;
-	scanf("%f", &score);
-	score > 100 ? printf("Out of Range\n") : score >= 80 ? printf("A\n") : score >= 70 ? printf("B\n") : score >= 60 ? printf("C\n") : score >= 50 ? printf("D\n") : score >= 0 ? printf("F\n") : printf("Out of Range\n");
-	return (0);
-}
+int	main()	{	float	score;	score = 0;	scanf("%f", &score);	score > 100 ? printf("Out of Range\n") : score >= 80 ? printf("A\n") : score >= 70 ? printf("B\n") : score >= 60 ? printf("C\n") : score >= 50 ? printf("D\n") : score >= 0 ? printf("F\n") : printf("Out of Range\n");	return (0); }
