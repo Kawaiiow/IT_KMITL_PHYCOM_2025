0658e2f9c2889392d5bfa942ede0b474a884993e
Kawaiiow <poohsn1357@gmail.com>
2025-08-20 10:49:50 +0700

    lift

---
+++ lab_08/lift.c
@@ -0,0 +1,42 @@
+#include "sys/types.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct	s_info
+{
+	uint	age;
+	double	weight;
+}	t_info;
+
+void	input(t_info *rec)
+{
+	scanf("\n%u %lf", &rec->age, &rec->weight);
+}
+
+int	main()
+{
+	t_info	*rec;
+	uint	n;
+	uint	limit;
+	double	sum;
+	uint	valid;
+
+	valid = 0;
+	sum = 0;
+	scanf("%u %u", &n, &limit);
+	rec = malloc(n * sizeof(t_info));
+	for (uint i = 0; i < n; i++)
+	{
+		input(rec + i);
+	}
+	for (uint i = 0; i < n; i++)
+	{
+		if (rec[i].age >= 12 && !valid)
+			valid = 1;
+		sum += rec[i].weight;
+	}
+	if (!valid || sum > limit)
+		printf("Not Safe\n");
+	else
+		printf("Safe\n");
+}
